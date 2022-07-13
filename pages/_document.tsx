c8ce10ce7467a1b35b4b2d138c473a8b583827f1
cobe <cobe@clover.games>
2022-07-13 18:26:27 +0900

    fix: styled-components 먹지 않는 과정 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,25 +1,42 @@
 import Document, {
-  DocumentContext,
-  DocumentInitialProps,
-  Head,
   Html,
+  Head,
   Main,
   NextScript,
+  DocumentContext,
 } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext,
-  ): Promise<DocumentInitialProps> {
-    const initialProps = await Document.getInitialProps(ctx);
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
 
-    return initialProps;
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
     return (
       <Html>
-        <Head />
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
