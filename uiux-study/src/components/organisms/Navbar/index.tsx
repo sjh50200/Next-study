314881fad87b993f8a7b685e90cc5228b73c6039
cobe <cobe@clover.games>
2023-04-10 13:45:55 +0900

    feat: styled-component 테스트용 navbar component 구현

---
+++ uiux-study/src/components/organisms/Navbar/index.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import React from 'react';
+import * as S from './style';
+
+const Navbar = () => {
+  return <S.NavbarContainer>Navbar</S.NavbarContainer>;
+};
+
+export default Navbar;
---
+++ uiux-study/src/components/organisms/Navbar/style.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const NavbarContainer = styled.nav`
+  background: rgba(0, 0, 0, 0.9);
+  height: 6rem;
+  color: white;
+`;
